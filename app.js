f5e64bcbcd267b1564bbd50ec5c0a7eca1fad286
Kavyan Patel <kavyanpatel1104@gmail.com>
2023-07-18 22:03:38 -0700

    final heroku?

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ var path = require("path");
 var bodyParser = require('body-parser');
 var helmet = require('helmet');
 var rateLimit = require("express-rate-limit");
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
