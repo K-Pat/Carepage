09c52fb113663200abf37177ff5a4e04c41e720d
Kavyan Patel <kavyanpatel1104@gmail.com>
2023-08-07 15:40:46 -0700

    Working Update and View Functions
    Very rudimentary some things to change include me always making the view details button available. Now I need to create a logout button and a more interactive UI.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { Client } = require('pg');
+const { Client, Pool } = require('pg');
 var express = require('express');
 var http = require('http');
 var path = require("path");
@@ -16,8 +16,11 @@ const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100 // limit each IP to 100 requests per windowMs
 });
-
 const connectionString = process.env.DATABASE_URL; // No need to assign, Heroku automatically sets this
+
+
+
+
 /*
 const client = new Client({
   connectionString: connectionString,
@@ -26,7 +29,6 @@ const client = new Client({
   }
 });
 */
-
 //WORKING CLIENT ABOVE WITHOUT CONDITIONAL HEROKU MODIFICATIONS
 
 
@@ -41,10 +43,71 @@ app.use(express.static(path.join(__dirname,'./public')));
 app.use(helmet());
 app.use(limiter);
 
+//table for username and passwords
 client.query('CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, username TEXT, email TEXT UNIQUE, password TEXT)', (err, res) => {
     if(err) throw err;
 });
 
+//table for session
+client.query(`
+    CREATE TABLE IF NOT EXISTS "session" (
+        "sid" varchar NOT NULL COLLATE "default",
+        "sess" json NOT NULL,
+        "expire" timestamp(6) NOT NULL
+    ) WITH (OIDS=FALSE);
+`, (err, res) => {
+    if (err) throw err;
+    console.log("Session table is set up!");
+});
+/*
+client.query('ALTER TABLE "session" ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid")', (err, res) => {
+  if(err) throw err;
+  console.log("Session table altered successfully");
+});
+*/
+const session = require('express-session');
+const pgSession = require('connect-pg-simple')(session);
+
+app.use(session({
+    store: new pgSession({
+        conString: connectionString,
+        tableName: 'session'
+    }),
+    secret: 'aVerySecretKey',  // Change this to a strong secret in production
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }  // 30 days
+}));
+
+//table for user details on homepage
+client.query('CREATE TABLE IF NOT EXISTS userDetails(userId INTEGER REFERENCES users(id), name TEXT, birthday DATE, address TEXT, phone TEXT)', (err, res) => {
+  if(err) throw err;
+});
+
+//client.query('ALTER TABLE userDetails ADD CONSTRAINT unique_userid UNIQUE (userid);');
+
+/*
+const setupDatabase = async () => {
+  const client = new Pool({
+    connectionString: connectionString,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  });
+  await client.connect();
+
+  // Add a unique constraint on userid
+  await client.query('ALTER TABLE userDetails ADD CONSTRAINT unique_userid UNIQUE (userid);');
+
+  await client.end();
+}
+
+setupDatabase().then(() => {
+  console.log("Database setup complete");
+}).catch((err) => {
+  console.error("Error setting up the database:", err);
+});
+*/
 app.get('/',(req,res) => {
   res.sendFile(path.join(__dirname,'./public/index.html'));
 });
@@ -96,7 +159,10 @@ app.post('/login', async (req, res) => {
       const passwordMatch = await bcrypt.compare(req.body.password, result.rows[0].password);
       if (passwordMatch) {
         let usrname = result.rows[0].username;
-        res.send(`<div align ='center'><h2>login successful</h2></div><br><br><br><div align ='center'><h3>Hello ${usrname}</h3></div><br><br><div align='center'><a href='./login.html'>logout</a></div>`);
+        // Inside the successful login section:
+        req.session.userId = result.rows[0].id;  // Store user ID in session
+        res.redirect(`/homepage.html?username=${usrname}`);
+
       } else {
         res.send("<div align ='center'><h2>Invalid email or password</h2></div><br><br><div align ='center'><a href='./login.html'>login again</a></div>");
       }
@@ -106,8 +172,88 @@ app.post('/login', async (req, res) => {
   }
 });
 
-// Remaining code with all db.* replaced with client.query or similar
-// Please note that carepage table has not been defined
+
+function ensureAuthenticated(req, res, next) {
+  if (req.session && req.session.userId) {
+    return next();
+}
+  return res.status(401).send('Unauthorized');
+}
+/*
+app.post('/updateDetails', ensureAuthenticated, async (req, res) => {
+  const { name, birthday, address, phone } = req.body;
+  const userId = req.session.userId;  // Retrieve user ID from session
+  
+  if (!userId) {
+    return res.send("User not logged in");
+  }
+  
+  client.query('INSERT INTO userDetails(userId, name, birthday, address, phone) VALUES ($1, $2, $3, $4, $5) ON CONFLICT(userId) DO UPDATE SET name = $2, birthday = $3, address = $4, phone = $5', [userId, name, birthday, address, phone], (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.send("Internal server error");
+    }
+    return res.send("Details updated successfully!");
+  });
+
+const values = [req.session.userId, req.body.name, req.body.birthday, req.body.address, req.body.phone];
+
+try {
+  res.redirect('/homepage.html');
+} catch (err) {
+  console.error('Error updating details:', err);
+  res.status(500).send('Internal Server Error');
+}
+
+});
+*/
+
+app.post('/updateDetails', ensureAuthenticated, async (req, res) => {
+  const { name, birthday, address, phone } = req.body;
+  const userId = req.session.userId;  // Retrieve user ID from session
+  
+  if (!userId) {
+    return res.send("User not logged in");
+  }
+  
+  client.query('INSERT INTO userDetails(userId, name, birthday, address, phone) VALUES ($1, $2, $3, $4, $5) ON CONFLICT(userId) DO UPDATE SET name = $2, birthday = $3, address = $4, phone = $5', [userId, name, birthday, address, phone], (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.json({ success: false });
+  }
+  
+  res.json({ success: true });
+  });
+});
+
+app.get('/viewDetails', ensureAuthenticated, async (req, res) => {
+  const userId = req.session.userId;
+  
+  if (!userId) {
+      return res.send('User not logged in');
+  }
+
+  try {
+      const result = await client.query('SELECT name, birthday, address, phone FROM userDetails WHERE userId = $1', [userId]);
+      if (result.rows.length === 0) {
+          return res.send('No details available for this user.');
+      }
+      
+      const userDetail = result.rows[0];
+      // Here, you can render a template or send the data as JSON to be rendered on the client side.
+      // For simplicity, I'll send the data as plain text.
+      return res.send(`
+          Name: ${userDetail.name} <br>
+          Birthday: ${userDetail.birthday} <br>
+          Address: ${userDetail.address} <br>
+          Phone: ${userDetail.phone}
+      `);
+  } catch (err) {
+      console.error('Error fetching details:', err);
+      return res.status(500).send('Internal Server Error');
+  }
+});
+
 
 var port = process.env.PORT || 3000;
 
